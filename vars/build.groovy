40d6aaecd6f410330628916864f84f6e7c0ec2bd
genchsusu <mingde.zhu@storehub.com>
2021-04-05 01:30:10 +0800

    fix

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,9 +3,15 @@ import com.libs.PodTemplates
 def call(BUILD_SLAVE=null, timeoutMinutes = 60 ) {
     def label = "k8sagent-${UUID.randomUUID().toString()}"
 
+
+    // 
     def containers = new PodTemplates().Create(BUILD_SLAVE)
 
-    podTemplate(label: label, containers: containers, volumes: [emptyDirVolume(mountPath: '/home/jenkins', memory: true)], imagePullSecrets: []) {
+    podTemplate(label: label, containers: containers, 
+                volumes: [emptyDirVolume(mountPath: '/home/jenkins', memory: true)], 
+                imagePullSecrets: [], 
+                showRawYaml: false) 
+    {
         node(label) {
             timeout(time: timeoutMinutes, unit: 'MINUTES') {
                 echo "the pipeline is executed in a k8s agent " + label + "!!!"
