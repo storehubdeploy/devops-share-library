ec09175592794ee8517e36851020666d1ea06bd9
genchsusu <mingde.zhu@storehub.com>
2021-04-04 23:54:47 +0800

    move to vars

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,17 @@
 import com.libs.ContainerTemplate
 
+@Simple 
+class Container {
+   String name
+   String image
+}
+ 
 def call(timeoutMinutes = 60 ) {
     def label = "k8sagent-${UUID.randomUUID().toString()}"
 
-    def xxxxx = [[name:"test1",image:"docker:stable-dind"],[name:"test2",image:"dtzar/helm-kubectl:2.12.0"],[name:"test3",image:"java:openjdk-8u111-jre-alpine"]]
+    def xxxxx = []
+    xxxxx.add(new Container(name: "test1",image:"docker:stable-dind")
+    xxxxx.add(new Container(name: "test2",image:"dtzar/helm-kubectl:2.12.0")
     def containerTemplates = ContainerTemplate.Create(xxxxx)
 
     podTemplate(label: label, containers: containerTemplates, volumes: [emptyDirVolume(mountPath: '/home/jenkins', memory: true)], imagePullSecrets: []) {
