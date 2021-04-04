0afd31e337cb7276f532c2ff4ed8fee2819f6cc3
genchsusu <mingde.zhu@storehub.com>
2021-04-05 01:34:34 +0800

    fix

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,10 +3,10 @@ import com.libs.PodTemplates
 def call(BUILD_SLAVE=null, timeoutMinutes = 60 ) {
     def label = "k8sagent-${UUID.randomUUID().toString()}"
 
-
-    // 
+    // Generate podTemplate
+    // https://plugins.jenkins.io/kubernetes/
+    // Pod and container template configuration
     def containers = new PodTemplates().Create(BUILD_SLAVE)
-
     podTemplate(label: label, containers: containers, 
                 volumes: [emptyDirVolume(mountPath: '/home/jenkins', memory: true)], 
                 imagePullSecrets: [], 
