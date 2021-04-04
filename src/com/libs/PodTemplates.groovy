5f8ecca3020203bab004cac25f11e83bdf5e363f
genchsusu <mingde.zhu@storehub.com>
2021-04-05 01:03:22 +0800

    fix

--- src/com/libs/PodTemplates.groovy
+++ src/com/libs/PodTemplates.groovy
@@ -4,15 +4,13 @@ def Create(def containers) {
     def containerTemplates = []
     for (def i:containers ) {
         def container = containerTemplate(container.name ?: '', container.image ?: '')
-        container.name              = i.name            ?: ''
-        container.image             = i.image           ?: ''
         container.command           = i.command         ?: 'cat'
         container.args              = i.args            ?: ''
         container.ttyEnabled        = i.ttyEnabled      ?: true
         container.privileged        = i.privileged      ?: true
         container.alwaysPullImage   = i.alwaysPullImage ?: false
         // Add containerTemplate to list, and then return
-        containerTemplates.add(containerTemplate)
+        containerTemplates.add(container)
 
         // // Add containerTemplate to list, and then return
         // containerTemplates.add(containerTemplate(name: i.name, image: i.image, command: 'cat',ttyEnabled: true , privileged: true, alwaysPullImage: false))
