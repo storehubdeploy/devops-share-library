52f16bcb0c2ce983d5dcdc226aec96a0956fab89
genchsusu <mingde.zhu@storehub.com>
2021-04-05 02:14:30 +0800

    fix

--- vars/build.groovy
+++ vars/build.groovy
@@ -15,8 +15,8 @@ def call(BUILD_SLAVE=null, timeoutMinutes = 60 ) {
         node(label) {
             timeout(time: timeoutMinutes, unit: 'MINUTES') {
                 echo "the pipeline is executed in a k8s agent " + label + "!!!"
-            }
 
+            }
         }
     }
 }
