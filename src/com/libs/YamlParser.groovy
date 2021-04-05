f2fb45c559121dcaa88ff37430605fbf119521b7
genchsusu <mingde.zhu@storehub.com>
2021-04-06 02:10:04 +0800

    update log

--- src/com/libs/YamlParser.groovy
+++ src/com/libs/YamlParser.groovy
@@ -1,51 +1,51 @@
-package com.libs
+// package com.libs
 
-@Grab('org.yaml:snakeyaml:1.17')
-import org.yaml.snakeyaml.Yaml
-
-
-public class YamlParser implements Serializable {
-
-    private Yaml yaml;
-    private def yamlObj;
+// @Grab('org.yaml:snakeyaml:1.17')
+// import org.yaml.snakeyaml.Yaml
 
 
+// public class YamlParser implements Serializable {
 
-    public YamlParser(String yamlText) {
-        yaml = new Yaml()
-        yamlObj = yaml.load(yamlText)
-    }
+//     private Yaml yaml;
+//     private def yamlObj;
 
-    public YamlParser() {
-        yaml = new Yaml()
-    }
 
-    private def dumpYamlObj(def yamlObj) {
-        return yaml.dump(yamlObj)
-    }
 
-    private getYamlValue(def yamlKeyPath) {
-        def localYamlObj = yamlObj
-        for (def keyPath : yamlKeyPath.split("/")) {
-            localYamlObj = localYamlObj[keyPath]
-            if (!localYamlObj)
-                break
-        }
-        return localYamlObj
-    }
+//     public YamlParser(String yamlText) {
+//         yaml = new Yaml()
+//         yamlObj = yaml.load(yamlText)
+//     }
 
-    public static def loadYaml(def yamlText, def yamlKeyPath = null) {
-        def parser = new YamlParser(yamlText)
-        if (yamlKeyPath) {
-            return parser.getYamlValue(yamlKeyPath)
-        } else {
-            return parser.yamlObj
-        }
-    }
+//     public YamlParser() {
+//         yaml = new Yaml()
+//     }
 
-    public static def dumpYaml(def yamlObj) {
-        def parser = new YamlParser()
-        return parser.dumpYamlObj(yamlObj)
-    }
+//     private def dumpYamlObj(def yamlObj) {
+//         return yaml.dump(yamlObj)
+//     }
 
-}
+//     private getYamlValue(def yamlKeyPath) {
+//         def localYamlObj = yamlObj
+//         for (def keyPath : yamlKeyPath.split("/")) {
+//             localYamlObj = localYamlObj[keyPath]
+//             if (!localYamlObj)
+//                 break
+//         }
+//         return localYamlObj
+//     }
+
+//     public static def loadYaml(def yamlText, def yamlKeyPath = null) {
+//         def parser = new YamlParser(yamlText)
+//         if (yamlKeyPath) {
+//             return parser.getYamlValue(yamlKeyPath)
+//         } else {
+//             return parser.yamlObj
+//         }
+//     }
+
+//     public static def dumpYaml(def yamlObj) {
+//         def parser = new YamlParser()
+//         return parser.dumpYamlObj(yamlObj)
+//     }
+
+// }
