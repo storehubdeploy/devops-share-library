27678a5a6a58053cd599ac5056ba8a5bfcdf2710
genchsusu <mingde.zhu@storehub.com>
2021-04-06 02:05:03 +0800

    update log

--- vars/build.groovy
+++ vars/build.groovy
@@ -83,27 +83,27 @@ def call(GIT_BRANCH=null,  GIT_PROJECT=null, DOCKER_REPO=null, BUILD_SLAVE=null 
 //         throw e
 //     }
 // }
-def gitFetch() {
-    def gitParams = [ Depth: 1,
-                Timeout: 600,
-                Result: "",
-                Credential: "45ffa5c8-48bf-4c18-b40f-334bc25d0c56" 
-                Url: "https://github.com/storehubdeploy/"
-              ]
+// def gitFetch() {
+//     def gitParams = [ Depth: 1,
+//                 Timeout: 600,
+//                 Result: "",
+//                 Credential: "45ffa5c8-48bf-4c18-b40f-334bc25d0c56" 
+//                 Url: "https://github.com/storehubdeploy/"
+//               ]
 
-    stage 'Git Fetch'
-    log.title("Start fetching code from git project: ${GIT_PROJECT} using the docker project: ${DOCKER_REPO}")
+//     stage 'Git Fetch'
+//     log.title("Start fetching code from git project: ${GIT_PROJECT} using the docker project: ${DOCKER_REPO}")
 
-    retry(3) {
-        timeout(time: gitParams.Timeout , unit: 'SECONDS') {
-            gitParams.Result = checkout([$class                           : 'GitSCM',
-                                   branches                         : [[name: GIT_BRANCH]],
-                                   doGenerateSubmoduleConfigurations: false,
-                                   extensions                       : [[$class: 'CloneOption', noTags: true, reference: '', shallow: true, depth: gitParams.Depth]],
-                                   submoduleCfg                     : [],
-                                   userRemoteConfigs                : [[credentialsId: gitParams.Credential, url: gitParams.Url + GIT_PROJECT + '.git']]])
-        }
-    }
+//     retry(3) {
+//         timeout(time: gitParams.Timeout , unit: 'SECONDS') {
+//             gitParams.Result = checkout([$class                           : 'GitSCM',
+//                                    branches                         : [[name: GIT_BRANCH]],
+//                                    doGenerateSubmoduleConfigurations: false,
+//                                    extensions                       : [[$class: 'CloneOption', noTags: true, reference: '', shallow: true, depth: gitParams.Depth]],
+//                                    submoduleCfg                     : [],
+//                                    userRemoteConfigs                : [[credentialsId: gitParams.Credential, url: gitParams.Url + GIT_PROJECT + '.git']]])
+//         }
+//     }
 
-    return gitParams.Result
-}
+//     return gitParams.Result
+// }
